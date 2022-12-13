d112996aecd0a65e88aae8cbcc212162bee97f2a
Mitchell Henschel <me@mitchellhenschel.com>
2022-12-13 11:05:57 -0500

    R practice

---
+++ R/Codwars3_replace.R
@@ -0,0 +1,11 @@
+duplicate_encode <- function(word){
+  chars = tolower(strsplit(word, "")[[1]])
+  dupes <- chars[duplicated(chars)]
+  result<-paste(ifelse(chars%in%dupes,")","("), collapse = "")
+  return(result)
+}
+
+duplicate_encode("din")
+duplicate_encode("recede")
+duplicate_encode("Success")
+duplicate_encode("(( @")
