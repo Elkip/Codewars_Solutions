dc1ff2770ca9e82243fe49da0dba5a710a24f6ba
Mitch <me@mitchellhenschel.com>
2022-12-13 22:10:51 -0500

    cleanup

--- R/Codewars1.R
+++ R/Codewars1.R
@@ -1,17 +1,17 @@
 divisors <- function(n) {
-  sum = 0
-  product = 0
-  j = 0
+  sum <- 0
+  product <- 0
+  j <- 0
   for (i in 1:n) {
-    product = i*j
-    j = 0
+    product <- i*j
+    j <- 0
     while (product <= n) {
-      j = j + 1
-      product = i*j
+      j <- j + 1
+      product <- i*j
       if (product == n) {
         print(i, j)
         if (i == j) {
-          sum = sum + 1
+          sum <- sum + 1
         } else {
           sum = sum + 2
         }
